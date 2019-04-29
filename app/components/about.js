553a0d330f805c019ee8a64e6f1fcca8fd3f77bb
hansen ling <hansenling@berkeley.edu>
2019-04-28 17:35:22 -0700

    added picture to about

--- app/components/about.js
+++ app/components/about.js
@@ -23,11 +23,13 @@ export default class About extends React.Component{
 				</div>
 			</section>
 
-			<div className = "jumbotron text-center">
+			<div className = "jumbotron text-center" style = {{fontSize:'2em'}}>
 				<p>1. Click register, enter name, password. Login using that info </p>
 				<p>2. To submit content. Get a spotify embed link. Go to a song or album, click ...->share->copy embed code. past in the song/playlist section. Enter in a title and text</p>
+				<p>
+				<div style = {{}}><img src="/embedcodeinstructions.png" alt=""/></div>
+				</p>
 				<p>3. You can search for a song, album, user, or artist. This will bring you to a global post about the song or album. This should have all data from posts about that topic</p>
-				<a href="/contact" className ="btn btn-lg btn-primary">Drop us a link</a>
 			</div>
 		</div>
 		);
