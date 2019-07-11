0f9b1e1c033afa1ba108850dcb3bb98c1ea98d60
Benji Lin <benjaleelin@gmail.com>
2019-07-11 00:39:05 -0700

    standard header code cleanup and touchups. dropdowns next

--- app/components/standard_header.js
+++ app/components/standard_header.js
@@ -73,7 +73,7 @@ class SearchItem extends React.Component {
 };
 
 class SearchList extends React.Component {
-	
+
 	constructor(props)
 	{
 		super(props);
@@ -94,21 +94,26 @@ class SearchList extends React.Component {
 	        },
 	        body: JSON.stringify({type: "search", text: input}),
 	    }).then(function(response) { return response.json();})
-	    .then(function (data) {    	
+	    .then(function (data) {
 	    	var search_results = SortSearch(data.users, data.artists, data.songs, data.albums);
 	    	that.search_list.current.clearItems();
 	    	for (var key in search_results)
 	        {
 	        	that.search_list.current.renderItem(search_results[key][0], search_results[key][1]);
 	        }
-	 	})	
+	 	})
 	}
 
 	render ()
 	{
 		return(
 			<div>
-				<input style = {{backgroundColor:'#178275', border:'2px solid white', height:'35px', borderRadius:'7px', paddingLeft: '8px'}} onChange={this.handleChange.bind(this)} placeholder = "Search" type='text' name='country' className='search_bar'/>
+				<input
+					className={'search_bar'}
+					onChange={this.handleChange.bind(this)}
+					placeholder='Search'
+					type='text'
+					/>
 				<SearchItem ref={this.search_list}/>
 			</div>
 		);
@@ -124,16 +129,11 @@ export default class StandardHeader extends React.Component {
 		this.search_list = React.createRef();
 		this.dropdownRef = React.createRef();
 		this.notificationsRef = React.createRef();
-  		this.dropdown_content = []
-  		this.optionsRef = React.createRef();
-	  	this.notification_div = ""
-	  	this.dropdown_div = ""
-	  	this.login_div = ""
-	  	if (this.props.username == undefined)
-	  	{
-			this.login_div = <div style = {{marginRight: '30px'}}><a style = {{color:'white', fontFamily:'RobotoRegular', fontSize:'18px'}} href="/login">Login</a></div>
-	  	}
-
+		this.dropdown_content = []
+		this.optionsRef = React.createRef();
+  	this.notification_div = ""
+  	this.dropdown_div = ""
+  	this.login_div = ""
 	}
 
 	logoutClicked ()
@@ -147,9 +147,9 @@ export default class StandardHeader extends React.Component {
 	        },
 	    })
 	    .then(function(response) { return response.json();})
-	    .then(function (data) {    	
+	    .then(function (data) {
 	    	location.reload(true);
-	 	})		
+	 	})
 	}
 
 
@@ -178,7 +178,7 @@ export default class StandardHeader extends React.Component {
 		this.forceUpdate();
 	}
 
-	componentDidMount() 
+	componentDidMount()
 	{
 		var that = this;
 	    fetch('/notifications', {
@@ -190,10 +190,10 @@ export default class StandardHeader extends React.Component {
 	        },
 	    })
 	    .then(function(response) { return response.json();})
-	    .then(function (data) {    	
+	    .then(function (data) {
 	    	that.props.notifications = data.notifications
 		  	if (that.props.notifications != undefined && that.props.notifications.length > 0)
-		  	{	
+		  	{
 			  	for (var i = 0; i < that.props.notifications.length; ++i)
 			  	{
 			  		var comment_text = that.props.notifications[i].num_comments + ", comments"
@@ -211,7 +211,7 @@ export default class StandardHeader extends React.Component {
 			  				comment_text = comment_text.substring(0, comment_text.indexOf(",")) + comment_text.substring(comment_text.indexOf(",") + 1, comment_text.length)
 			  			}
 			  		}
-			  		var notification_text = "Your post " + that.props.notifications[i].name + " got " + likes_text + comment_text 
+			  		var notification_text = "Your post " + that.props.notifications[i].name + " got " + likes_text + comment_text
 			  		var notification_url = "/user/" + that.props.notifications[i].username + "/" + that.props.notifications[i].post_id
 			  		if (that.props.notifications[i].tag > 0)
 			  		{
@@ -224,9 +224,9 @@ export default class StandardHeader extends React.Component {
 			  			{
 			  				notification_url = "/user/" + that.props.notifications[i].name + "/" + that.props.notifications[i].post_id
 			  			}
-			  			
+
 			  		}
-			  		that.dropdown_content.push(<div key = {that.props.notifications[i].post_id} id = {that.props.notifications[i].post_id} className = "dropdownelement" style = {{background:'white', border: '1px solid black'}}> 
+			  		that.dropdown_content.push(<div key = {that.props.notifications[i].post_id} id = {that.props.notifications[i].post_id} className = "dropdownelement" style = {{background:'white', border: '1px solid black'}}>
 			  			<a className = "dropdownelement" href = {notification_url} >{notification_text} </a>
 			  			 <button key = {that.props.notifications[i].post_id} style = {{right:'0px', position:'absolute', height:'25px'}} className = "dropdownelement" onClick = {that.removeNotification.bind(that, that.props.notifications[i].post_id)}> X </button>
 			  			</div>)
@@ -234,19 +234,19 @@ export default class StandardHeader extends React.Component {
 			  	that.notification_div = <div className = "notifications" ref = {that.notificationsRef} onClick = {that.toggleNotifications.bind(that)} style = {{marginRight: '10px', fontWeight:'bold', fontSize:'12pt', color: "#178275", width:'24px', height:'24px', textAlign:'center', backgroundColor:'white', borderRadius:'50%'}}> {that.props.notifications.length}</div>
 		  	}
 		  	that.forceUpdate()
-	 	})		
+	 	})
 		window.addEventListener('mousedown', this.handleClickOutside.bind(this));
 	}
 
-	componentWillUnmount() 
+	componentWillUnmount()
 	{
 		window.removeEventListener('mousedown', this.handleClickOutside.bind(this));
 	}
 
-	handleClickOutside(event) 
+	handleClickOutside(event)
 	{
 		if (event.type == 'contextmenu')
-		{ 
+		{
 			return
 		}
 		if (event.target.className != "dropdown" && event.target.className != "notifications" && event.target.className != "dropdownelement") {
@@ -285,8 +285,8 @@ export default class StandardHeader extends React.Component {
 	        body: JSON.stringify({}),
 	    })
 	    .then(function(response) { return response.json();})
-	    .then(function (data) {    	
-	 	})		
+	    .then(function (data) {
+	 	})
 	}
 
 	toggleOptions()
@@ -306,81 +306,52 @@ export default class StandardHeader extends React.Component {
 		this.optionsRef.current.style.display = 'none'
 	}
   render() {
+		this.dropdown_div = <div className = "dropdown" ref = {this.dropdownRef} style = {{ width:'400px', minHeight:'10px', position: "fixed", right:'220px', top:'50px', background:'white', display:'none', fontWeight:'normal', fontSize:'12pt', zIndex:'8'}}>{this.dropdown_content}</div>
 
-  	
-
-	this.dropdown_div = <div className = "dropdown" ref = {this.dropdownRef} style = {{ width:'400px', minHeight:'10px', position: "fixed", right:'220px', top:'50px', background:'white', display:'none', fontWeight:'normal', fontSize:'12pt', zIndex:'8'}}>{this.dropdown_content}</div>
-
-
-  	var user_login = <div>
-				        <a style = {{color:'white', fontFamily:'RobotoRegular', fontSize:'18px'}} href = {"/user/" + this.props.username}> {this.props.username} </a>
-  			   		</div>
-  	var logout_register = <div style = {{marginRight: '10px', fontFamily:'RobotoRegular', fontSize:'18px'}}><a href="/" style = {{color:'white'}} onClick = {this.logoutClicked.bind(this)}>Logout</a></div>
-  	if (this.props.username == undefined)
-  	{
-	  	user_login = <div style = {{marginRight: '10px'}}>
-			        <a style = {{color:'white', fontFamily:'RobotoRegular', fontSize:'18px'}} href="/register" >Register</a>			        
-					</div>
-		logout_register = ""
-
-  	}
-
+  	let user_login, login_div;
+  	if (this.props.username == undefined) {
+	  	user_login = (
+				<div className="headerLink" onClick={() => {window.location.href = "/register"}}>Register</div>
+			);
+			login_div = <div className="headerLink" onClick={() => {window.location.href = "/login"}}>Login</div>
+  	} else {
+			user_login = (
+				<div className="headerLink" onClick={() => {window.location.href = "/user/" + this.props.username}}>{this.props.username}</div>
+			);
+		}
 
     return (
-
-	<div className = "StandardHeader" >
-		<head>
-			<title>Unheard</title>
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
-
-		</head>
-		<header>
-			<div className = "id" headercontainer = "reacttest" style = {{top:'0px', left:'0px', position:'fixed', height:'60px', backgroundColor: '#178275', width:'100%', zIndex:'5'}}>
-				<div className = "home" style = {{position:'absolute',top:'15px', left:'25px', fontFamily:'RobotoRegular', fontSize:'18px'}}><a href="/" style = {{color:'white'}}>Home</a></div>
-			    <div className = "searchbarcontainer" style = {{position:'absolute',top:'12px', left:'100px'}}>
-			        <div className="search_list" style = {{overflow: 'auto'}}>
-			        <SearchList />
-			        </div>
-			    </div>
-			    <div className = "headerlinks" style = {{display:'flex', flexDirection:'row', position:'absolute', right:'20px', marginRight: '10px', top:'15px', fontFamily:'RobotoRegular'}}>
-			    	{this.login_div}
-				    <div className = "userlogin" style = {{position:'relative', marginRight:'10px'}}>
-				    	{user_login}
-				    </div >
-
-					{this.notification_div}
-					{this.dropdown_div}
-					<div style = {{width:'20px', height:'10px'}}>
+			<div>
+				<head>
+					<title>Unheard</title>
+					<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+				</head>
+				<header>
+					<div className="fixedHeader">
+						<div className="headerLink" onClick={() => {window.location.href = "/"}}>Home</div>
+						<div className="searchBarContainer">
+							<div className="search_list" style={{overflow: 'auto'}}>
+								<SearchList />
+							</div>
+						</div>
+						{login_div}
+						{user_login}
+						{this.notification_div}
+						{this.dropdown_div}
+						<div className="headerLink" onClick={() => {window.location.href = "/random"}}>Random</div>
+						<div className="headerMenuArrow">
+							<svg className = "options" onClick = {this.toggleOptions.bind(this)} width="18" height="16" viewBox="0 0 26 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M23.0926 1.25L13 14.3606L2.90742 1.25L23.0926 1.25Z" fill="white" stroke="white" strokeWidth="2" />
+							</svg>
+						</div>
+						<div ref = {this.optionsRef} style = {{backgroundColor: '#178275', display:'none', position:'fixed', right:'0px', top:'40px'}}>
+							<div className="headerLink" onClick={() => {window.location.href = "/about"}}>About</div>
+							<div className="headerLink" onClick={() => {window.location.href = "/contact"}}>Contact</div>
+							{this.props.username != null && <div className="headerLink" onClick={this.logoutClicked.bind(this)}>Logout</div>}
+						</div>
 					</div>
-				    <div className = "random" >
-				    	
-				    	<a style ={{marginRight: '30px', position:'relative', color:'white', fontFamily:'RobotoRegular', fontSize:'18px'}}href="/random">Random</a>
-			    	</div>
-			    	<div style = {{position:'relative', top:'7px'}} className = "options">
-						<svg className = "options" onClick = {this.toggleOptions.bind(this)} width="18" height="16" viewBox="0 0 26 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M23.0926 1.25L13 14.3606L2.90742 1.25L23.0926 1.25Z" fill="white" stroke="white" strokeWidth="2"/>
-						</svg>
-
-				    </div>			
-			    	<div ref = {this.optionsRef} style = {{backgroundColor: '#178275', display:'none', position:'fixed', right:'0px', top:'40px'}}>
-
-				    	<div className = "about" >
-							<a className = "about" style ={{color:'white', fontFamily:'RobotoRegular', fontSize:'18px'}}href="/about">About</a>
-						</div>
-						<div className = "contact" >
-							<a className = "contact"  style = {{color:'white', fontFamily:'RobotoRegular', fontSize:'18px'}} href="/contact">Contact</a>
-						</div>
-					    <div className = "logoutregister" style = {{}}>
-					    	{logout_register}
-					    </div >
-				    </div>		
-				</div>
+				</header>
 			</div>
-
-		</header>
-	</div>
-
-	);
+		);
+	}
 }
-}
-
