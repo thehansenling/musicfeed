2adb5b96ea6eaea8554158cbb4f092733e7c2198
hansen ling <hansenling@berkeley.edu>
2019-07-13 20:09:35 -0700

    Whoopsie, back to prod db

--- app/server/app.js
+++ app/server/app.js
@@ -34,22 +34,22 @@ var PRIORITY_MODIFIER = 8640 / 4500
 var score_sql = " " + SCORE_MODIFIER + " * LOG(ABS(cast(likes as signed) - cast(dislikes as signed))) * SIGN(cast(likes as signed) - cast(dislikes as signed)) + (relevant_timestamp - UNIX_TIMESTAMP() * 1000)/45000000 "
 
 //test database
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-01.cleardb.net',
-  user     : 'bc7ebf9f6de242',
-  password : 'aa9b1c1f',
-  database : 'heroku_cdc4ca7b10e1680',
-  multipleStatements: true
-});
-
-//prod database
 // var connection = mysql.createConnection({
-//   host     : 'us-iron-auto-sfo-03-bh.cleardb.net',
-//   user     : 'b82ff0c686544a',
-//   password : '52ad3adb',
-//   database : 'heroku_4df94195b1d1e6b',
+//   host     : 'us-cdbr-iron-east-01.cleardb.net',
+//   user     : 'bc7ebf9f6de242',
+//   password : 'aa9b1c1f',
+//   database : 'heroku_cdc4ca7b10e1680',
 //   multipleStatements: true
 // });
+
+//prod database
+var connection = mysql.createConnection({
+  host     : 'us-iron-auto-sfo-03-bh.cleardb.net',
+  user     : 'b82ff0c686544a',
+  password : '52ad3adb',
+  database : 'heroku_4df94195b1d1e6b',
+  multipleStatements: true
+});
 
 var spotify_username = '44a9442188734ab2999542562c6477c3';
 var spotify_password = '9c24d61cf7234c4a80f6f2f49ecc9c45';
